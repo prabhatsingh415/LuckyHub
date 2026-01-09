2c50263b90b4fb4b63c3917741178ec90eb3d4ca
Prabhat Singh <prabhatsinghrajput795@gmail.com>
2026-01-09 21:53:28 +0530

    Fix: fixed the google sign in button's hover effect.

--- Frontend/src/components/Form.jsx
+++ Frontend/src/components/Form.jsx
@@ -212,7 +212,7 @@ export default function Form({
           <button
             type="button"
             onClick={handleAuthWithGoogle}
-            className="w-full flex items-center justify-center gap-2 border px-4 py-2 rounded-md hover:bg-gray-100 mt-4 dark:border-gray-700"
+            className="w-full flex items-center justify-center gap-2 border px-4 py-2 rounded-md hover:bg-gray-100 hover:text-black  mt-4 dark:border-gray-700"
           >
             <svg className="w-5 h-5" viewBox="0 0 24 24">
               <path
